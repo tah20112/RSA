ff20a8dfe972ae317db37a840885b09c323b7ac6
Tom Heale <tah20112@gmail.com>
2016-11-29 07:13:27 -0500

    Commenting & planning

---
+++ functions.js
@@ -0,0 +1,13 @@
+/* Our collection of functions for RSA encryption */
+
+function bigPrime(){
+    /* Using random number generation and Fermat's little
+     * theorum, this function will find and output 2 
+     * VERY large prime numbers for our RSA key. */
+
+}
+
+function lilFermat(){
+    /* if p is prime  */
+
+}
