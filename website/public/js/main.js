b019c6c8227c08ec3559a965a42a979992ffe3b3
billmwong <billmwong@gmail.com>
2016-12-06 12:18:09 -0500

    cleaned up obsolete code

--- website/public/js/main.js
+++ website/public/js/main.js
@@ -6,7 +6,6 @@ var handleError = function(err) {
 
 app.controller("mainController", function ($scope, $http) {
   $scope.formData = {};
-  $scope.editInputs = {};
   $scope.display = {text: "poop"};
 
   $scope.newMessage = function() {
@@ -17,48 +16,4 @@ app.controller("mainController", function ($scope, $http) {
       })
       .error(handleError);
   };
-
-  $scope.newTodo = function() {
-    $http.post('/api/todos/new', $scope.formData)
-      .success(function(thisTodo) {
-        // CLear the form and update the todos
-        $scope.formData = {};
-        $scope.todos.push(thisTodo);
-      })
-      .error(handleError);
-  };
-
-  $scope.deleteTodo = function (todo) {
-    $http.post('api/todos/delete', todo)
-      .success(function (newTodos) {
-        $scope.todos = newTodos;  
-      })
-      .error(handleError);
-  };
-
-  $scope.toggleComplete = function (id) {
-    $http.post('api/todos/toggleComplete', {id: id})
-      .success(function (newTodos) {
-        $scope.todos = newTodos;  
-      })
-      .error(handleError);
-  };
-
-  $scope.openEdit = function (id, text) {
-    // Make the edit input show the current text
-    $scope.editInputs[id] = text
-  };
-
-  $scope.editTodo = function (id) {
-    // Edit the text of the mongo entry and reload the todos
-    $http.post('api/todos/edit', {
-      id: id,
-      newText: $scope.editInputs[id]
-    })
-      .success(function (newTodos) {
-        $scope.todos = newTodos;  
-      })
-      .error(handleError);
-  };
-
 });
